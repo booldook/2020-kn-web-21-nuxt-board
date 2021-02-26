87b603f01048ed5c066e14625b4d5333f8ace8da
booldook <booldook@gmail.com>
2021-02-26 15:23:15 +0900

    ...

--- views/pages/index.vue
+++ views/pages/index.vue
@@ -1,8 +1,12 @@
 <template lang="pug">
 	.list-wrapper
-		v-data-table(:headers="headers" :items="list" :items-per-page="2" class="elevation-1")
+		.title-wrapper
+			h2 Nuxt / Vue를 활용한 게시판
+		v-data-table(:headers="headers" :items="list" :items-per-page="2" class="elevation-1" @click:row="handleClick")
 </template>
 <script>
+import moment from 'moment'
+
 export default {
 	name: 'list',
 	layout: 'board',
@@ -11,24 +15,45 @@ export default {
 			headers: [
 				{ text: '번호', align: 'center', sortable: true, value: 'id',},
 				{ text: '제목', value: 'title' },
-				{ text: '작성자', value: 'writer' },
-				{ text: '작성일', value: 'created' },
-				{ text: '첨부파일', value: 'savefile' },
-				{ text: '조회수', value: 'readnum' },
+				{ text: '작성자', value: 'writer', align: 'center' },
+				{ text: '작성일', value: 'wdate', align: 'center' },
+				{ text: '첨부파일', value: 'orifile', align: 'center' },
+				{ text: '조회수', value: 'readnum', align: 'center' },
 			]
 		}
 	},
-	async asyncData({ $axios, store }) {
+	async asyncData({ $axios, store, params, query }) {
 		try {
-			let { data: list } = await $axios.get('http://127.0.0.1:3000/api/list')
+			let { data } = await $axios.get('http://127.0.0.1:3000/api/list')
+			console.log(data)
+			let list = data.map(v => {
+				v.wdate = moment(v.created).format('YYYY-MM-DD')
+				return v
+			});
 			return { list }
 		}
 		catch(e) {
 			console.log(e)
 		}
 	},
+	methods: {
+		handleClick(e) {
+			console.log(e.id)
+		}
+	}
 }
 </script>
 <style lang="scss">
-	
+	.title-wrapper {
+		text-align: center;
+		color: $colorDarker;
+		padding: 1em;
+		margin: 2em 0;
+	}
+	table {
+		td {
+			text-align: center;
+			&:nth-child(2) {text-align: left;}
+		}
+	}
 </style>
